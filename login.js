89cd900096f3768cb3a09b4ce9116ead90fee9bc
Gustavo <gustavolaureth22@gmail.com>
2023-04-25 10:06:11 -0300

    updates done

--- login.js
+++ login.js
@@ -1,6 +1,9 @@
 let password = document.getElementById('password')
 let button = document.getElementById('button')
 let eye = document.getElementById('eye')
+var text1 = document.getElementById("password");
+var text2 = document.getElementById("name");
+var butt1 = document.getElementById("button1");
 
 function ShowPassword() {
     if (password.type == 'password') {
@@ -13,3 +16,15 @@ function ShowPassword() {
         eye.classList.add('bi-eye')
     }
 }
+
+function check() {
+
+  if (text1.value == null || text1.value == "" && (text2.value == null || text2.value == "")) {
+    butt1.style.backgroundColor = "#edf1f5";
+    butt1.style.color = "#000";
+  }
+  else {
+    butt1.style.backgroundColor = "#1890ff";
+    butt1.style.color = "#edf1f5";
+  }
+};
